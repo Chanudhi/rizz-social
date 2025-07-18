ae7bb0f77a2d409240ffd6fa95bd721658e5418b
Chanudhi <aluchanu29@gmail.com>
2025-07-18 12:37:00 +0530

    feat: update Sidebar component and enhance styling with Irish Grover font

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
-import { Home, User, Users, MessageCircle, Plus, LogOut } from "lucide-react";
+import { Flame, User, Users, MessageCircle, Plus, LogOut } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <div className="w-60 bg-black h-full text-white flex flex-col gap-6 p-6">
-      <h2 className="text-2xl font-bold">Rizz</h2>
-      <NavLink to="/home" className="flex gap-2 items-center"><Home size={18}/> Trending</NavLink>
+    <div className="w-60 bg-neutral-900 h-full text-white flex flex-col gap-6 p-6">
+      <h2 className="text-5xl font-bold font-irishgrover">Rizz</h2>
+      <NavLink to="/home" className="flex gap-2 items-center"><Flame size={18}/> Trending</NavLink>
       <NavLink to="/profile" className="flex gap-2 items-center"><User size={18}/> Profile</NavLink>
       <NavLink to="/friends" className="flex gap-2 items-center"><Users size={18}/> Friends</NavLink>
       <NavLink to="/messages" className="flex gap-2 items-center"><MessageCircle size={18}/> Messages</NavLink>
