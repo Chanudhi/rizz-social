f06d5e4ea16f22a84fd293c4fd4801e059d875e3
Chanudhi <aluchanu29@gmail.com>
2025-07-20 00:45:34 +0530

    Add: alignmrnts

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
     <div className="fixed top-0 left-0 h-screen w-60 bg-neutral-900 text-white flex flex-col justify-between p-6"> {/* Sidebar container */}
       <div className="flex flex-col gap-7"> {/* Sidebar Top content */}
         <NavLink to="/" ><h2 className="text-5xl font-regular font-irishgrover pl-4">Rizz</h2></NavLink> {/* Title with Irish Grover font */}
-        <nav className="flex flex-col gap-4 pl-2">
+        <nav className="flex flex-col gap-4 pl-2 font-bold"> {/*Navigation links */}
           <NavLink to="/" className="flex gap-3 items-center"><Flame size={18}/> Trending</NavLink>
           <NavLink to="/#" className="flex gap-3 items-center"><Compass size={18}/>Explore</NavLink>
           <NavLink to="/friends" className="flex gap-3 items-center"><Users size={18}/> Friends</NavLink>
